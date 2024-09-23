eceed3460c7dbbf03f4151229673ff7321ebf8dc
Lucas Morato <lukmorato@gmail.com>
2024-09-23 11:20:58 +0200

    8

--- 2.Modulos-Nativos/8.ls-advanced.js
+++ 2.Modulos-Nativos/8.ls-advanced.js
@@ -1,14 +1,53 @@
-const fs = require('node:fs')
+const fs = require('node:fs/promises')
+const path = require('node:path')
 
 const folder = process.argv[2] ?? '.'
 
-fs.readdir(folder,(err, files)=>{
-    if (err){
-        console.error('Error de directorio', err);
-        return
-    }
 
-    files.forEach(file =>{
-        console.log(file);
+// fs.readdir(folder,(err, files)=>{
+//     if (err){
+//         console.error('Error de directorio', err);
+//         return
+//     }
+
+//     files.forEach(file =>{
+//         console.log(file);
+//     })
+// })
+
+
+async function ls(folder) {
+    let files
+    try{
+        files = await fs.readdir(folder)
+    } catch{
+        console.error(`No se ha podido leer el directorio ${folder}`);
+        process.exit(1)
+    }
+    const filesPromises = files.map(async file =>{
+        const filePath = path.join(folder, file)
+        let stats
+        try{
+            stats = await fs.stat(filePath) // Info del archivo   
+        } catch{
+            comsole.error(`No se ha podido leer el archivo ${filePath}`)
+            process.exit(1)
+        }
+
+        const isDirectory = stats.isDirectory()
+        const fileType = isDirectory ? 'd':'-'
+        const fileSize = stats.size.toString()
+        const fileModified = stats.mtime.toLocaleString()
+
+        return ` ${fileType} ${file} ${fileSize.toString()} ${fileModified}`
     })
-})
+
+    const filesInfo = await Promise.all(filesPromises)
+    
+    filesInfo.forEach(fileInfo => {
+        console.log(fileInfo);
+    });
+
+}
+
+ls(folder)
